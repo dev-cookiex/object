f7cfb0b283e3645096143c1f86f2700a26ce43ad
Tony <tony.js@zoho.eu>
2021-01-18 17:16:40 -0300

    feat: best code

--- index.ts
+++ index.ts
@@ -5,9 +5,5 @@ export const omit = <O, K extends string | number | symbol>( object: O, ...keys:
   ) as Omit<O, K>
 }
 
-export const pick = <O, K extends keyof O>( object: O, ...keys: K[] ): Pick<O, K> => {
-  return Object.fromEntries(
-    Object.entries( object )
-      .filter( ( [ key ] ) => keys.includes( key as K ) )
-  ) as Pick<O, K>
-}
+export const pick = <O, K extends keyof O>( object: O, ...keys: K[] ): Pick<O, K> =>
+  keys.reduce( ( data, key ) => ( { ...data, [key]: object[key] } ), {} as Pick<O, K> )
